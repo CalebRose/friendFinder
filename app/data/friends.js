7b969ed20bc973992ce8658860c0206c5ec6c3f1
CalebRose <ctrose17@gmail.com>
2019-06-02 21:46:41 -0500

    Express system complete; need to finish algorithm

---
+++ app/data/friends.js
@@ -0,0 +1,138 @@
+var friends = [{
+    "name": "Spike Spiegel",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Spike_Spiegel_as_drawn_by_the_creators.jpg/220px-Spike_Spiegel_as_drawn_by_the_creators.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "",
+    "photo": "",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+var friends = require("./../data/friends.js");
+
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
+
+    app.post("/api/reservations", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newFriend = req.body;
+
+        // We then add the json the user sent to the character array
+        friends.push(newFriend);
+
+        // We then display the JSON to the users
+        res.json(newFriend);
+    });
+};
