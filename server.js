6cef1f280e1fb61eb9c893e9b57c4ff6dfc3536b
CalebRose <ctrose17@gmail.com>
2019-06-12 18:23:00 -0500

    Fixed issue where style.css wasn't loading; fixed issue where svg image wasn't properly routed

--- server.js
+++ server.js
@@ -10,23 +10,31 @@ var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(bodyParser.text());
-app.use(bodyParser.json({
-    type: 'application/vnd.api+json'
-}));
+app.use(
+  bodyParser.json({
+    type: "application/vnd.api+json"
+  })
+);
 
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true
-}));
+  })
+);
 
 app.use(express.json());
+
+app.use("/public", express.static(path.join(__dirname, "./app/public")));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
